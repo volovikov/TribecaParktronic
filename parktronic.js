2f642751fe4b27fcbc41603002fcb07f30d59dab
volovikov <v.volovikov@gmail.com>
2020-05-22 22:03:49 +0300

    Работает два индикатора

--- parktronic.js
+++ parktronic.js
@@ -1,6 +1,6 @@
-var leftSonar = require('sonar').connect({
-    outputPin: P0,
-    inputPin: P1
+var centerRightSonar = require('sonar').connect({
+    outputPin: A1,
+    inputPin: A0
 });
 
 var rightSonar = require('sonar').connect({
@@ -10,10 +10,39 @@ var rightSonar = require('sonar').connect({
 
 var display = require('display').connect(Serial3);
 
-leftSonar.ping(function(dest) {
-  console.log('Sonar Left: ' + dest);
-});
-rightSonar.ping(function(dest) {
-  console.log('Sonar Right: ' + dest);
-  display.distance(dest.toFixed(2));
-});
+
+var sonarPingFrequency = 500;
+var minDistance = 5;
+var meteringResult = [5, 5, 5, 5];
+
+var showMinDistance = function() {
+  var dest = Math.min.apply(null, meteringResult);
+  display.showMinDistance(dest.toFixed(2));
+};
+
+var rightSonarPing = function(nextFn) {
+  rightSonar.ping(function(dest) {
+    meteringResult[0] = dest;
+    display.showDistanceIndicator('sensorRight', dest.toFixed(2));
+    nextFn && nextFn();
+  });
+};
+
+var centerRightSonarPing = function(nextFn) {  
+  centerRightSonar.ping(function(dest) {
+    meteringResult[1] = dest;
+    display.showDistanceIndicator('sensorCenterR', dest.toFixed(2));
+    nextFn && nextFn();
+  });
+};
+
+setInterval(function() {
+  rightSonarPing(centerRightSonarPing(showMinDistance));
+}, sonarPingFrequency);
+
+
+
+
+
+
+
